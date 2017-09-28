010ab6fe9f4c6780855fb44b9e2ac71fddb51e7c
Oleg Gutsalov <alega19@mail.ru>
2017-09-29 01:24:10 +1000

    psycopg2==2.7.3.1 and fixed install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "START"
 apt-get update
 
-apt-get install -y python-software-properties software-properties-common postgresql-9.6 postgresql-client-9.6 postgresql-contrib-9.6
+apt-get install -y python-software-properties software-properties-common postgresql-9.6 postgresql-client-9.6 postgresql-contrib-9.6 postgresql-server-dev-9.6
 /etc/init.d/postgresql start
 apt-get install -y sudo
 sudo -u postgres createdb hasker
@@ -17,8 +17,9 @@ pip install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip install uwsgi
-pip install -r requirements/production.txt
+pip install -r requirements/test.txt
 python manage.py test hasker.tests -p=*.py --settings=config.settings.test
+python manage.py migrate
 python manage.py runserver 0.0.0.0:80 --settings=config.settings.local
 echo "END"
 
