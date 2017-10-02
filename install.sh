5975a1c04e3e2e17f5a51d1792ff023ef6525764
Oleg Gutsalov <alega19@mail.ru>
2017-10-03 00:40:23 +1000

    removed unnecessary packages

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 echo "START"
-NGCONF="server {\n
-    listen 80;\n
-    server_name 0.0.0.0;\n
-    charset utf-8;\n
-    client_max_body_size 2M;\n
-\n
-    location /media {\n
-        alias /usr/local/hasker/media;\n
-    }\n
-\n
-    location /static {\n
-        alias /usr/local/hasker/static;\n
-    }\n
-\n
-    location / {\n
-        include uwsgi_params;\n
-        uwsgi_pass 127.0.0.1:8000;\n
-    }\n
-}\n"
+NGCONF="server {
+    listen 80;
+    server_name 0.0.0.0;
+    charset utf-8;
+    client_max_body_size 2M;
+
+    location /media {
+        alias /usr/local/hasker/media;
+    }
+
+    location /static {
+        alias /usr/local/hasker/static;
+    }
+
+    location / {
+        include uwsgi_params;
+        uwsgi_pass 127.0.0.1:8000;
+    }
+}"
 mkdir -p /usr/local/hasker/{media,static};
 
 apt-get update
 
-apt-get install -y python-software-properties software-properties-common postgresql-9.6 postgresql-client-9.6 postgresql-contrib-9.6 postgresql-server-dev-9.6
+apt-get install -y postgresql-9.6 postgresql-contrib-9.6
 /etc/init.d/postgresql start
 apt-get install -y sudo
 sudo -u postgres createdb hasker
@@ -45,5 +45,5 @@ rm /etc/nginx/sites-enabled/default
 echo $NGCONF > /etc/nginx/sites-enabled/hasker.conf
 /etc/init.d/nginx start
 
-uwsgi --socket 127.0.0.1:8000 --wsgi-file config/wsgi.py 
+uwsgi --socket 127.0.0.1:8000 --wsgi-file config/wsgi.py
 echo "END"
